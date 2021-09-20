1503be7a1681128fde7a9bf1f7d0f13bfe0dfc21
Samuel Honorato <samuel.silva@deliverycenter.com>
2021-09-20 19:50:46 -0300

    feat: adiciona teste com verify_on_exit

--- test/mock_api_test.exs
+++ test/mock_api_test.exs
@@ -1,38 +1,58 @@
 defmodule Services.Mode1.MockApiTest do
   use ExUnit.Case
 
+  import Mox
+
   require Logger
   import ExUnit.CaptureLog
 
+  describe "get_mock/1 with setup" do
+    setup :verify_on_exit!
+
+    test "success with expect and verify/0" do
+      expect(Services.Mode1.MockApiMock, :request, 1, fn _url -> {:ok, "OK"} end)
+
+      assert Services.Mode1.MockApi.get_mock(200) == {:ok, "OK"}
+    end
+  end
+
   describe "get_mock/1" do
-    test "success with expect and verify" do
-      Mox.expect(Services.Mode1.MockApiMock, :request, 1, fn _url -> {:ok, "OK"} end)
+    test "success with expect and verify/0" do
+      expect(Services.Mode1.MockApiMock, :request, 1, fn _url -> {:ok, "OK"} end)
 
       assert Services.Mode1.MockApi.get_mock(200) == {:ok, "OK"}
 
-      Mox.verify!(Services.Mode1.MockApiMock)
+      verify!()
+    end
+
+    test "success with expect and verify/1" do
+      expect(Services.Mode1.MockApiMock, :request, 1, fn _url -> {:ok, "OK"} end)
+
+      assert Services.Mode1.MockApi.get_mock(200) == {:ok, "OK"}
+
+      verify!(Services.Mode1.MockApiMock)
     end
 
     test "success" do
-      Mox.stub(Services.Mode1.MockApiMock, :request, fn _url -> {:ok, "OK"} end)
+      stub(Services.Mode1.MockApiMock, :request, fn _url -> {:ok, "OK"} end)
 
       assert Services.Mode1.MockApi.get_mock(200) == {:ok, "OK"}
     end
 
     test "success response default" do
-      Mox.stub_with(Services.Mode1.MockApiMock, Services.Mode1.MockApi.ResponseDefault)
+      stub_with(Services.Mode1.MockApiMock, Services.Mode1.MockApi.ResponseDefault)
 
       assert Services.Mode1.MockApi.get_mock(200) == {:ok, "DEFAULT"}
     end
 
 #    test "success with api" do
-#      Mox.stub_with(Services.Mode1.MockApiMock, Services.Mode1.MockApi)
+#      stub_with(Services.Mode1.MockApiMock, Services.Mode1.MockApi)
 #
 #      assert Services.Mode1.MockApi.get_mock(200) == {:ok, %{"title" => "Hello..."}}
 #    end
 
     test "success with log" do
-      Mox.stub(Services.Mode1.MockApiMock, :request, fn url ->
+      stub(Services.Mode1.MockApiMock, :request, fn url ->
         Logger.error("Getting request: #{url}")
 
         {:ok, "OK"}
@@ -43,20 +63,20 @@ defmodule Services.Mode1.MockApiTest do
 
     test "success with expect" do
       Services.Mode1.MockApiMock
-      |> Mox.expect(:request, fn _url -> {:ok, "OK"} end)
+      |> expect(:request, fn _url -> {:ok, "OK"} end)
 
       assert Services.Mode1.MockApi.get_mock(200) == {:ok, "OK"}
     end
 
     test "success with process" do
       Services.Mode1.MockApiMock
-      |> Mox.expect(:request, fn _url -> {:ok, "OK"} end)
+      |> expect(:request, fn _url -> {:ok, "OK"} end)
 
       parent_pid = self()
 
       task =
         Task.async(fn ->
-          Mox.allow(Services.Mode1.MockApi, parent_pid, _task_async_pid = self())
+          allow(Services.Mode1.MockApi, parent_pid, _task_async_pid = self())
 
           assert Services.Mode1.MockApi.get_mock(200) == {:ok, "OK"}
         end)
